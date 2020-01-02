ce624d6a2c653b1fc4f5aa13a2f6a47061b22caf
BuildTools <psolebeck@gmail.com>
2020-01-02 02:02:44 +0100

    intital commit

--- getfromvitadb.py
+++ getfromvitadb.py
@@ -2,15 +2,19 @@ import requests
 import datetime
 import json
 r = requests.get('https://rinnegatamante.it/vitadb/list_hbs_json.php')
-j = json.reads(r.content)
+j = json.loads(r.content)
 for entry in j:
+  f = open("files/" + entry["titleid"] + ".json")
+  fi = f.read()
+
+  jsonbefore = json.loads(fi)
+
+  x = datetime.datetime.now()
   dlcount = entry["downloads"]
   name = entry["name"]
   id = entry["titleid"]
-  f = open("files/" + id + ".json")
-  jsonbefore = json.reads(f.read())
+  tosave = {"downloads":dlcount,"name":name,"titleid":id,"time":x.strftime("%m/%d/%Y, %H:%M:%S")}
+  jsonbefore.append(tosave)
   f.close()
-  x = datetime.datetime.now()
-  tosave = {"downloads":dlcount,"name":name,"titleid":id,"time":x}
-  jsonbefore += tosave
-  print(jsonbefore)
+  f = open("files/" + entry["titleid"] + ".json","w")
+  f.write(json.dumps(jsonbefore))
